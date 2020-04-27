fb99ad077d216ec26cf4d1bba54f6fc0df61e611
Terence Cole <trcexcel@yahoo.com>
2020-04-27 09:22:14 -0500

    Updated JS with button clicks

--- tabs.js
+++ tabs.js
@@ -1,1 +1,9 @@
-console.log('IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role = "tab"]');
+const tabPanel = tabs.querySelectorAll('[role = "tabpanel"]');
+
+function handleTabClick(event) {
+  console.log(event.currentTarget);
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
